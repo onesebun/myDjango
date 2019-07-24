ae6ce712272a11295dc0d05d9a10e6dc1f0f77b8
Your Name <you@example.com>
2019-07-24 21:32:03 +0800

    update

--- mblog/mainsite/views.py
+++ mblog/mainsite/views.py
@@ -1,6 +1,26 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.template.loader import get_template
 from django.shortcuts import render
+from django.http import HttpResponse
+from datetime import datetime
+from .models import Post
+import sys
+
+reload(sys)
+sys.setdefaultencoding("utf-8")
 
 # Create your views here.
+def homepage(request):
+    # posts = Post.objects.all()
+    # post_lists = list()
+    # for count, post in enumerate(posts):
+    #     post_lists.append("No.{}:".format(str(count)) + str(post) + "<hr>")
+    #     post_lists.append("<small>" + str(post.body.encode('utf-8')) + "</small><br><br>")
+    # return HttpResponse(post_lists)
+    template = get_template('index.html')
+    posts = Post.objects.all()
+    now = datetime.now()
+    html = template.render(locals())
+    return HttpResponse(html)
--- mblog/mblog/settings.py
+++ mblog/mblog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'ewc_hr$w#)g0i!t$p&%p6211h(e)_ea6&g6)8$6u^8qkxeulvh'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.1.171']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'mainsite',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'mblog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -105,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Taipei'
 
 USE_I18N = True
 
